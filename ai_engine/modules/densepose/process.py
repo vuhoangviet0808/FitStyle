55cf3d3842d7fcc277b11bddae9c3664bf8235bc
unknown <vuotngokhong@gmail.com>
2025-02-07 01:42:41 +0700

    Chua chay duoc DensePose

--- ai_engine/modules/densepose/process.py
+++ ai_engine/modules/densepose/process.py
@@ -1,21 +1,61 @@
 import os
 import json
-from ai_engine.modulers.pose_estimatio.openpose.process import get_keypoints_from_openpose
-from ai_engine.modules.pose_estimation.smpl.smpl import map_keypoints_to_3d
+import subprocess
+import numpy as np
+from ai_engine.modules.pose_estimation.process import get_keypoints_from_openpose
 
-def process_image(image_path):
+IMAGE_DIR = os.path.abspath("./storage/input")
+OUTPUT_DIR = os.path.abspath("./storage/output")
+
+
+def run_densepose(image_path, keypoints_file):
     base_name = os.path.splitext(os.path.basename(image_path))[0]
+    image_folder = os.path.abspath(os.path.join(IMAGE_DIR, base_name))
+    output_folder = os.path.abspath(os.path.join(OUTPUT_DIR, base_name))
+    command = [
+        "docker", "run", "--rm",
+        "-v", f"{image_folder}:/workspace/input",
+        "-v", f"{output_folder}:/workspace/output",
+        "-e", "CUDA_VISIBLE_DEVICES=-1",
+        "-v","densepose:/densepose/tools", 
+        "garyfeng/densepose",
+        "python","/densepose/tools/infer.py",
+        "--im", f"/workspace/input/{base_name}.jpg",
+        "--output-dir", "/workspace/output"
+    ]
 
-    keypoints_2d = get_keypoints_from_openpose(image_path)
+    try:
+        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("DensePose done!")
+        print("stdout:", result.stdout.decode())
+        print("stderr:", result.stderr.decode())
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
 
-    if keypoints_2d is None:
-        print(f"❌ Không có keypoints từ OpenPose cho ảnh {image_path}!")
-        return None
+    
+def process_image_for_densepose(image_path):
+    base_name = os.path.splitext(os.path.basename(image_path))[0]
+    image_folder = os.path.abspath(os.path.dirname(image_path))
+    output_folder = os.path.join(OUTPUT_DIR, base_name)
+    keypoints_json = get_keypoints_from_openpose(image_path, False)
+    print(image_folder)
+    print(output_folder)
+    if not image_path or not image_folder or not keypoints_json:
+        print("Invalid input!")
+        return
+    
+    run_densepose(image_path, keypoints_json)
+    densepose_output = os.path.join(output_folder,  f"{base_name}_densepose.json")
+    if not os.path.exists(densepose_output):
+        print(f"Not found file 3D result from DensePose")
+        return
+    
+    with open(densepose_output, 'r') as f:
+        densepose_data = json.load(f)
 
-    keypoints_3d = map_keypoints_to_3d(keypoints_2d)
-    output_dir = os.path.abspath(os.path.dirname(image_path))
-    keypoints_file = os.path.join(output_dir, f"{base_name}_3dkeypoints.json")
-    with open(keypoints_file, 'w') as f:
-        json.dump(keypoints_3d, f)
+    print("3D result from DensePose:")
+    print(densepose_data)
 
-    return keypoints_file
+if __name__ == "__main__":
+    image_path = os.path.abspath("./storage/input/person2/person2.jpg")
+    process_image_for_densepose(image_path)
--- ai_engine/modules/pose_estimation/process.py
+++ ai_engine/modules/pose_estimation/process.py
@@ -2,18 +2,20 @@ import os
 import json
 import subprocess
 
-IMAGE_DIR = os.path.abspath("../../../storage/input")
-OUTPUT_DIR = os.path.abspath("../../../storage/output")
-MODEL_DIR = os.path.abspath("./openpose/models")
+IMAGE_DIR = os.path.abspath("./storage/input")
+OUTPUT_DIR = os.path.abspath("./storage/output")
+MODEL_DIR = os.path.abspath("./ai_engine/modules/pose_estimation/openpose/models")
 
 #Ham chay openpose len tat ca file anh trong 1 folder
 def run_openpose(image_path):
-    # base_name = os.path.splitext(os.path.basename(image_path))[0]
+    base_name = os.path.splitext(os.path.basename(image_path))[0]
     image_folder = os.path.abspath(os.path.dirname(image_path))
+    output_folder = os.path.abspath(os.path.join(OUTPUT_DIR, base_name))
+    
     command = [
         "docker", "run", "--rm",
         "-v", f"{image_folder}:/workspace/input",
-        "-v", f"{image_folder}:/workspace/output",
+        "-v", f"{output_folder}:/workspace/output",
         "-v", f"{MODEL_DIR}:/workspace/models",
         "mvdoc/openpose-cpu",
         "/openpose/build/examples/openpose/openpose.bin",
@@ -32,27 +34,33 @@ def run_openpose(image_path):
         print(f"❌ Lỗi khi chạy OpenPose: {e}")
 
 #Ham tim keypoints cua 1 anh
-def get_keypoints_from_openpose(image_path):
+def get_keypoints_from_openpose(image_path, take_keypoints=False):
     base_name = os.path.splitext(os.path.basename(image_path))[0]
-    image_folder = os.path.abspath(os.path.dirname(image_path))
-    run_openpose(image_path)
-    keypoints_file = os.path.join(image_folder, f"{base_name}_keypoints.json")
+    image_folder = os.path.abspath(os.path.join(IMAGE_DIR, base_name))
+    output_folder = os.path.abspath(os.path.join(OUTPUT_DIR, base_name))
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
+    keypoints_file = os.path.join(output_folder, f"{base_name}_keypoints.json")
+    if not os.path.exists(keypoints_file):
+        run_openpose(image_path)
+        
     if not os.path.exists(keypoints_file):
         print(f"❌ Không tìm thấy file keypoints cho ảnh {image_path}.")
         return None
     
     with open(keypoints_file, 'r') as f:
         keypoints_2d = json.load(f)
-
-    return keypoints_2d
+    if take_keypoints:
+        return keypoints_2d
+    return keypoints_file
 
 
 
 
 
 if __name__ == "__main__":
-    image_path = "../../../storage/person2/person2.jpg"  
+    image_path = "../../../storage/input/person2/person2.jpg"  
     keypoints = get_keypoints_from_openpose(image_path)
     if keypoints:
         print("✅ Keypoints 2D đã được lấy thành công!")
